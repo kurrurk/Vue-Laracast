0841cbc5e82e6d9c20a77b6d9a2a6eae22f30167
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-18 12:56:45 +0200

    13-Lifecycle Hooks, Fake APIs, and AJAX

---
+++ 13-Lifecycle Hooks, Fake APIs, and AJAX/js/components/Assigments.js
@@ -0,0 +1,45 @@
+import AssigmentList from "./AssigmentList.js";
+import AssigmentCreate from "./AssigmentCreate.js";
+
+export default {
+  components: { AssigmentList, AssigmentCreate },
+  template: `
+        <section class="space-y-6">
+            <AssigmentList :assignments="filters.inProgress" title="In Progress"/>
+            <AssigmentList :assignments="filters.completed" title="Completed"/>
+
+            <AssigmentCreate @add="add"/>
+        </section>
+    `,
+  data() {
+    return {
+      assignments: [],
+    };
+  },
+  computed: {
+    filters() {
+      return {
+        inProgress: this.assignments.filter(
+          (assignment) => !assignment.complete,
+        ),
+        completed: this.assignments.filter((assignment) => assignment.complete),
+      };
+    },
+  },
+  created() {
+    fetch("http://localhost:3001/assignments")
+      .then((response) => res.json(response))
+      .then((data) => {
+        this.assignments = data;
+      });
+  },
+  methods: {
+    add(newAssignment) {
+      this.assignments.push({
+        name: newAssignment,
+        complete: false,
+        id: this.assignments.length + 1,
+      });
+    },
+  },
+};
